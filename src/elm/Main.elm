c5c8438cbd25a138d77c8f87f3fde497d1dc0961
Connor Smallman <connor.smallman@bedegaming.com>
2016-11-20 09:49:49 +0000

    Added start app

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,58 +1,29 @@
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+-- Read more about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/user_input/buttons.html
 
--- APP
-main : Program Never Int Msg
+import Html exposing (beginnerProgram, div, button, text)
+import Html.Events exposing (onClick)
+
+
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  beginnerProgram { model = 0, view = view, update = update }
 
 
--- MODEL
-type alias Model = Int
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
 
-model : number
-model = 0
+
+type Msg = Increment | Decrement
 
 
--- UPDATE
-type Msg = NoOp | Increment
-
-update : Msg -> Model -> Model
 update msg model =
   case msg of
-    NoOp -> model
-    Increment -> model + 1
+    Increment ->
+      model + 1
 
-
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+    Decrement ->
+      model - 1
